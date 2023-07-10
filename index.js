edfd75c7e1b91900aa83715ed7b2fcaa0bd61a76
RashadGhanem <rashadghanem.rgh@gmail.com>
2023-07-10 17:57:32 +0300

    new

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ class Book {
     }
 }
 
-// Library class
 class Library {
     constructor() {
         this.books = [];
@@ -30,7 +29,6 @@ class Library {
     }
 }
 
-// Create instances of Book and Library
 const book1 = new Book("book1", "Rashad", 1);
 const book2 = new Book("book2", "Rihaj", 2);
 const book3 = new Book("book3", "Rayan", 3);
@@ -42,7 +40,7 @@ library.addBook(book2);
 library.addBook(book3);
 library.addBook(book4);
 
-// Handle form submission
+
 const form = document.getElementById("searchForm");
 const searchResults = document.getElementById("searchResults");
 
